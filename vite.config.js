d7f0e6233b31918def9257508b148c783d526b4f
Josh Vanderberg <jvanderberg@gmail.com>
2023-12-13 22:36:30 +0000

    add build

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,8 @@
-import legacy from '@vitejs/plugin-legacy';
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
-
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [react(), legacy()],
+	plugins: [react()],
 	server: {
 		port: 3000,
 		host: '0.0.0.0'
@@ -20,7 +18,25 @@ export default defineConfig({
         threads: true
     
     },
-	build: {
-		sourcemap: true
-	}
+    build: {
+        lib: {
+            // Could also be a dictionary or array of multiple entry points
+            entry: new URL('./src/index.ts', import.meta.url).pathname,
+            name: 'ReactLayout',
+            // the proper extensions will be added
+            fileName: 'react-box-layout',
+        },
+        rollupOptions: {
+          // make sure to externalize deps that shouldn't be bundled
+          // into your library
+          external: ['react', 'react-dom', 'react/jsx-runtime'],
+          output: {
+            // Provide global variables to use in the UMD build
+            // for externalized deps
+            globals: {
+              react: 'React',
+            },
+          },
+        }
+    }
 });
